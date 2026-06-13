1dd0b4fa56868fc8fa13efb928d06e21da4bf298
NELSON UGAZ <nelsonugazortiz@gmail.com>
2026-06-12 20:06:53 -0500

    Fix mixed content forcing HTTPS

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -21,13 +21,13 @@ class AppServiceProvider extends ServiceProvider
     {
         \Illuminate\Support\Facades\View::composer('*', function ($view) {
             $view->with('categories', \App\Models\Category::all());
-            
+
             // Inyectar sedes activas y sede seleccionada globalmente
             $view->with('global_headquarters', \App\Models\Headquarter::where('is_active', true)->get());
-            
+
             $selHqId = session('selected_headquarter_id');
             $view->with('selected_headquarter', $selHqId ? \App\Models\Headquarter::find($selHqId) : null);
-            
+
             if (\Illuminate\Support\Facades\Schema::hasTable('settings')) {
                 $view->with('contact_whatsapp', \App\Models\Setting::get('contact_whatsapp', '950664655'));
                 $view->with('contact_email', \App\Models\Setting::get('contact_email', 'hola@gourmetica.pe'));
